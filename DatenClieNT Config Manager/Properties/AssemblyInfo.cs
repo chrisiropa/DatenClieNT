f79302cf6b60c0b24f08a5234ef857ca720b0924
Christof Goletzko <c.goletzko@iropa.de>
2025-10-02 15:04:07 +0200

    2

--- DatenClieNT Config Manager/Properties/AssemblyInfo.cs
+++ DatenClieNT Config Manager/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.621")]
-[assembly: AssemblyFileVersion("1.0.1.621")]
+[assembly: AssemblyVersion("1.0.1.622")]
+[assembly: AssemblyFileVersion("1.0.1.622")]
